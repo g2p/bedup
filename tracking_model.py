7f51ab9644d0a6bfb842f63afb3e12adb406cfdf
Gabriel <g2p.code@gmail.com>
2012-10-01 18:22:59 +0200

    MiniHash entity

--- tracking_model.py
+++ tracking_model.py
@@ -1,10 +1,12 @@
 
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.sql import and_
 from sqlalchemy.ext.declarative import declarative_base, declared_attr
+from sqlalchemy.types import (UnicodeText, Integer, Binary)
 from sqlalchemy.util import generic_repr
 from sqlalchemy import (
-    UnicodeText, Integer, Binary, Column, ForeignKey, UniqueConstraint)
+    Column, ForeignKey, UniqueConstraint)
 
 
 def FK(cattr, primary_key=False, backref=None, nullable=False):
@@ -15,6 +17,24 @@ def FK(cattr, primary_key=False, backref=None, nullable=False):
             primary_key=primary_key,
             nullable=nullable),
         relationship(cattr.parententity, backref=backref))
+
+
+def FK2(entity, primary_key=False, backref=None, nullable=False):
+    # We could also return a composite column rather than multiple columns,
+    # but that may require a dynamically built class for the column type.
+    def gen_fk_cols():
+        for col in entity.__table__.primary_key:
+            yield Column(
+                col.type, ForeignKey(col),
+                primary_key=primary_key,
+                nullable=nullable)
+    fk_cols = list(gen_fk_cols())
+    join_conditions = list(
+        col1==col2
+        for (col1, col2) in zip(entity.__table__.primary_key, fk_cols))
+    rel = relationship(
+        entity, backref=backref, primaryjoin=and_(*join_conditions))
+    return fk_cols, rel
 
 
 # XXX I actually need create_or_update here
@@ -48,6 +68,11 @@ class InodeAndSize(Base):
 
     def __repr__(self):
         return 'InodeAndSize(inode=%r, size=%r)' % (self.inode, self.size)
+
+
+class MiniHash(Base):
+    (fs_id, inode), ias = FK2(InodeAndSize, primary_key=True)
+    mini_hash = Column(Binary, nullable=False)
 
 META = Base.metadata
 
