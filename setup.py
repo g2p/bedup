d965bc4d0540fec7e2ce8b0717bb09ecb52d068a
Gabriel <g2p.code@gmail.com>
2012-10-03 20:12:26 +0200

    Distutils and tox

---
+++ setup.py
@@ -0,0 +1,21 @@
+
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='bedup',
+    version='0.0.1',
+    author='Gabriel de Perthuis',
+    author_email='g2p.code@gmail.com',
+    description='Deduplication for Btrfs filesystems',
+    install_requires=[
+        #'cffi',  # 0.4 is not released and confuses pip
+        'pyxdg',
+        'sqlalchemy',
+    ],
+    packages=[
+        'bedup',
+    ],
+)
+
