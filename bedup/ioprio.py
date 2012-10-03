473221a4008636d2315219161ee4ff0fd7f3b351
Gabriel <g2p.code@gmail.com>
2012-10-03 21:52:27 +0200

    Another enum

--- bedup/ioprio.py
+++ bedup/ioprio.py
@@ -10,6 +10,11 @@ ffi.cdef('''
 #define IOPRIO_WHO_PGRP ...
 #define IOPRIO_WHO_USER ...
 
+#define IOPRIO_CLASS_NONE ...
+#define IOPRIO_CLASS_RT ...
+#define IOPRIO_CLASS_BE ...
+#define IOPRIO_CLASS_IDLE ...
+
 int ioprio_get(int which, int who);
 int ioprio_set(int which, int who, int ioprio);
 int IOPRIO_PRIO_VALUE(int class, int data);
@@ -17,16 +22,25 @@ int IOPRIO_PRIO_CLASS(int mask);
 int IOPRIO_PRIO_DATA(int mask);
 ''')
 
-# nabbed from schedutils/ionice.c
+# Parts nabbed from schedutils/ionice.c
 # include/linux/ioprio.h has the macro half
 lib = ffi.verify('''
+#include <unistd.h>
 #include <sys/syscall.h>
+
 #define IOPRIO_CLASS_SHIFT      (13)
 #define IOPRIO_PRIO_VALUE(class, data) (((class) << IOPRIO_CLASS_SHIFT) | data)
 #define IOPRIO_PRIO_MASK        ((1UL << IOPRIO_CLASS_SHIFT) - 1)
 #define IOPRIO_PRIO_CLASS(mask) ((mask) >> IOPRIO_CLASS_SHIFT)
 #define IOPRIO_PRIO_DATA(mask)  ((mask) & IOPRIO_PRIO_MASK)
 #define IOPRIO_PRIO_VALUE(class, data) (((class) << IOPRIO_CLASS_SHIFT) | data)
+
+enum {
+    IOPRIO_CLASS_NONE,
+    IOPRIO_CLASS_RT,
+    IOPRIO_CLASS_BE,
+    IOPRIO_CLASS_IDLE,
+};
 
 enum {
     IOPRIO_WHO_PROCESS = 1,
@@ -43,8 +57,6 @@ static inline int ioprio_get(int which, int who) {
 }
 ''')
 
-IOPRIO_IDLE_CLASS = 3
-
 
 def set_idle_priority(pid=None):
     """
@@ -57,5 +69,5 @@ def set_idle_priority(pid=None):
         pid = os.getpid()
     lib.ioprio_set(
         lib.IOPRIO_WHO_PROCESS, pid,
-        lib.IOPRIO_PRIO_VALUE(IOPRIO_IDLE_CLASS, 0))
+        lib.IOPRIO_PRIO_VALUE(lib.IOPRIO_CLASS_IDLE, 0))
 
