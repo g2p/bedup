b9d826dc9fb5ba56df9a6696aea438cf49c149ec
Gabriel <g2p.code@gmail.com>
2012-10-06 10:03:31 +0200

    Compile CFFI modules at installation time.

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@
 
 from distutils.core import setup
 
+import bedup.btrfs
+import bedup.chattr
+import bedup.fiemap
+import bedup.ioprio
+import bedup.openat
+
 setup(
     name='bedup',
     version='0.0.1',
@@ -14,6 +20,13 @@ setup(
         #'cffi >= 0.4',  # 0.4 is not released and confuses pip
         'pyxdg',
         'sqlalchemy',
+    ],
+    ext_modules=[
+        bedup.btrfs.ffi.verifier.get_extension(),
+        bedup.chattr.ffi.verifier.get_extension(),
+        bedup.fiemap.ffi.verifier.get_extension(),
+        bedup.ioprio.ffi.verifier.get_extension(),
+        bedup.openat.ffi.verifier.get_extension(),
     ],
     packages=[
         'bedup',
