4b3464c6a1fc73f217a93e57cc4a85353aa51e1d
Gabriel <g2p.code@gmail.com>
2012-10-08 19:41:19 +0200

    License headers

--- bedup/openat.py
+++ bedup/openat.py
@@ -1,3 +1,21 @@
+# bedup - Btrfs deduplication
+# Copyright (C) 2012 Gabriel de Perthuis <g2p.code+bedup@gmail.com>
+#
+# This file is part of bedup.
+#
+# bedup is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# bedup is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with bedup.  If not, see <http://www.gnu.org/licenses/>.
+
 from cffi import FFI
 import os
 
--- bedup/tracking.py
+++ bedup/tracking.py
@@ -1,4 +1,22 @@
 # vim: set fileencoding=utf-8 sw=4 ts=4 et :
+# bedup - Btrfs deduplication
+# Copyright (C) 2012 Gabriel de Perthuis <g2p.code+bedup@gmail.com>
+#
+# This file is part of bedup.
+#
+# bedup is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# bedup is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with bedup.  If not, see <http://www.gnu.org/licenses/>.
+
 import collections
 import errno
 import fcntl
