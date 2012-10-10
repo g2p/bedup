8101de1c26bfb48497c6cb5907ee7f46c93c5a2a
Gabriel <g2p.code@gmail.com>
2012-10-11 01:15:14 +0200

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import bedup.openat
 
 setup(
     name='bedup',
-    version='0.0.2dev',
+    version='0.0.2',
     author='Gabriel de Perthuis',
     author_email='g2p.code+bedup@gmail.com',
     url='https://github.com/g2p/bedup',
