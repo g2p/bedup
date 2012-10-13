3db279a162d884e919883e0e98f2504fadae608a
Gabriel <g2p.code@gmail.com>
2012-10-13 23:05:32 +0200

    Release 0.0.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if version_info < (2, 7):
 
 setup(
     name='bedup',
-    version='0.0.3dev',
+    version='0.0.3',
     author='Gabriel de Perthuis',
     author_email='g2p.code+bedup@gmail.com',
     url='https://github.com/g2p/bedup',
