43198281515b43a66fc595cf9f5a44f3814f533d
Gabriel <g2p.code@gmail.com>
2013-06-16 14:26:01 +0200

    Fix blkid regexp with no label, thanks Maeyanie
    Partially revert "device names can contain colons"
    (546dbed82891493b3ce399dc68f42290a2c3daab), closes #21.

--- bedup/filesystem.py
+++ bedup/filesystem.py
@@ -602,8 +602,8 @@ class WholeFS(object):
 
 
 BLKID_RE = re.compile(
-    br'^(?P<dev>/dev/.*):\s+'
-    br'(?:LABEL="(?P<label>[^"]*)"|\s+UUID="(?P<uuid>[^"]*)")+\s*$')
+    br'^(?P<dev>/dev/.*):'
+    br'(?:\s+LABEL="(?P<label>[^"]*)"|\s+UUID="(?P<uuid>[^"]*)")+\s*$')
 
 
 def is_subvolume(btrfs_mountpoint_fd):
